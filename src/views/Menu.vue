6119dd4b9994eab3e9be63e3aeeebe31c2f798bf
Josh Holladay <josh.holladay@mrm-mccann.com>
2019-01-23 19:47:38 -0700

    Feat (style) mealplans page

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -3,11 +3,17 @@
         <v-layout>
             <h1>Menu Page</h1>
         </v-layout>
+            <home-plans></home-plans>
     </v-container>
 </template>
 <script>
+import HomePlans from '@/components/HomePlans';
+
 export default {
-    name: 'Menu'
+    name: 'Menu',
+    components: {
+      HomePlans
+    }
 };
 </script>
 <style scoped>
