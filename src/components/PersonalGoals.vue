5234c93d751be66b656268db704e52373a63ab92
Josh Holladay <josh.holladay@mrm-mccann.com>
2019-01-29 23:29:18 -0700

    Feat (style) add personal goals panel

---
+++ src/components/PersonalGoals.vue
@@ -0,0 +1,127 @@
+<template>
+    <!-- <v-container fluid> -->
+        <div class="personal-goals-section">
+            <div class="section-labels">
+              <h3>PERSONAL GOALS</h3>
+              <h5>Current Goal Sprint: <span>Feb 1, 2019 - Apr 15, 2019</span></h5>
+            </div>
+            <div class="goal-container">
+              <div class="goal-tile water">
+                <div class="label">WATER</div>
+                <div class="data-container">
+                  <div class="percent">32%</div>
+                  <div class="accum">672</div>
+                </div>
+              </div>
+            </div>
+            <div class="goal-container">
+              <div class="goal-tile miles">
+                <div class="label">MILES</div>
+                <div class="data-container">
+                  <div class="percent">12%</div>
+                  <div class="accum">451</div>
+                </div>
+              </div>
+            </div>
+            <div class="goal-container">
+              <div class="goal-tile minutes">
+                <div class="label">MINUTES</div>
+                <div class="data-container">
+                  <div class="percent">32%</div>
+                  <div class="accum">223</div>
+                </div>
+              </div>
+            </div>
+            <div class="goal-container">
+              <div class="goal-tile meals">
+                <div class="label">MEALS</div>
+                <div class="data-container">
+                  <div class="percent">22%</div>
+                  <div class="accum">51</div>
+                </div>
+              </div>
+            </div>
+            
+        </div>
+    <!-- </v-container> -->
+</template>
+<script>
+export default {
+    name: 'PersonalGoals'
+};
+</script>
+<style lang="scss" scoped>
+.personal-goals-section {
+  // border: 1px solid lightgray;
+  padding: 20px;
+  background: rgb(238, 238, 238);
+  display: block;
+  border-radius: 5px;
+  box-shadow: -1px 2px 3px rgba(0,0,0,0.2);
+  .section-labels {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    align-items: flex-start;
+    margin: 0 0 20px 0;
+    h3 {
+      margin-top: -5px;
+    }
+    h5 {
+      span {
+        color: rgb(147, 147, 255);
+      }
+    }
+  }
+  .goal-container {
+    display: inline-block;
+    margin: 0 20px 0 0;
+    .goal-tile {
+      // background: lightgray;
+      padding: 8px;
+      // height: 50px;
+      width: 200px;
+      border-radius: 5px;
+      .label {
+        text-align: center;
+        color: #fff;
+        font-weight: 900;
+        font-size: 10px;
+        letter-spacing: 2px;
+        margin-bottom: 3px;
+      }
+      .data-container {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-around;
+        align-items: center;
+        color: #fff;
+        font-size: 26px;
+        // .percent {
+        //   // display: inline-block;
+        // }
+        // .accum {
+        //   // display: inline-block;
+        // }
+      }  
+    }
+  }
+  .water {
+      // background: linear-gradient(to right, rgb(23, 189, 245), rgb(87, 213, 255));
+      background: rgb(23, 189, 245);
+    }
+    .miles {
+      background: orange;
+      // background: linear-gradient(black, gray)
+    }
+    .minutes {
+      background: purple;
+      // background: linear-gradient(black, gray)
+    }
+    .meals {
+      background: rgb(4, 139, 76);
+      // background: linear-gradient(black, gray)
+    }
+}
+
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,17 +1,28 @@
 <template>
     <v-container id="dashboard" fluid>
-        <v-layout>
+        <v-layout class="page-title">
             <h1>Dashboard</h1>
         </v-layout>
+        <personal-goals />
     </v-container>
 </template>
 <script>
+import PersonalGoals from '@/components/PersonalGoals';
+
 export default {
-    name: 'Dashboard'
+    name: 'Dashboard',
+    components: {
+        PersonalGoals
+    }
 };
 </script>
-<style scoped>
+<style lang="scss" scoped>
 #dashboard {
-    background: rgb(115, 215, 255);
+    // background: lightgreen;
+    .page-title {
+        padding: 0;
+        margin: 0 0 20px 0;
+        // background: rgb(115, 215, 255);
+    }
 }
 </style>
